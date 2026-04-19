fc5ca77b87399a3dd2f4a140847d8330836a9642
Dylansius11 <dylansius.putra@gmail.com>
2026-04-19 18:09:02 +0700

    Already do the T-02 with deleting the anchor/lokal-core/test_initialize file first

---
+++ anchor/programs/lokal-core/src/lib.rs
@@ -0,0 +1,21 @@
+pub mod constants;
+pub mod error;
+pub mod instructions;
+pub mod state;
+
+use anchor_lang::prelude::*;
+
+pub use constants::*;
+pub use instructions::*;
+pub use state::*;
+
+declare_id!("4F2xbVhpy1idLj5FDdKPpRW1t7shYd21okXCSwyaxmoQ");
+
+#[program]
+pub mod anchor {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        initialize::handler(ctx)
+    }
+}
